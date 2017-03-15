6bd46de3108117a48d21760bf1f8c7c554acf453
Juris Tihomirovs <Juris Tihomirovs>
2017-03-15 20:29:48 +0200

    Hello world

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 // setup ports
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = 8080;
+var server_ip_address = '0.0.0.0';
 
 app.get('/', function (req, res) {
     res.status('200').send('Service is up');
