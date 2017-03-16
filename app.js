1c0a6e4db36e1627b5e3e7a12580e07537b52094
Juris Tihomirovs <Juris Tihomirovs>
2017-03-16 21:51:19 +0200

    First test

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
+var router = require("./router/router.js");
 
 // setup ports
 var server_port = 8080;
 var server_ip_address = '0.0.0.0';
 
-app.get('/', function (req, res) {
-    res.status('200').send('Service is up');
-});
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use("/", router);
 
 // server listens in on port
 app.listen(server_port, server_ip_address, function () {
